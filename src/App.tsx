2f9de3daa91d806e952308fe4ffdef3efef125ff
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-04 08:36:35 +0100

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
